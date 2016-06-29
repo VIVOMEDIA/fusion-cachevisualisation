4ba0de261ac47f8ee9dcbd10de55ab9b4cfc3c00
Denny Lubitz <lubitz@vivomedia.de>
2016-06-29 17:44:17 +0200

    Aspect to wrap content cache segments with additional informations

--- Classes/VIVOMEDIA/TypoScript/CacheVisualisation/Aspects/ContentCacheVisualisationAspect.php
+++ Classes/VIVOMEDIA/TypoScript/CacheVisualisation/Aspects/ContentCacheVisualisationAspect.php
@@ -5,6 +5,7 @@ namespace VIVOMEDIA\TypoScript\CacheVisualisation\Aspects;
 
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Object\Configuration\Configuration;
 
 /**
  * @Flow\Aspect()
@@ -16,54 +17,74 @@ class ContentCacheVisualisationAspect
     const TYPE_CACHED = 'cached';
     const TYPE_UNCACHED = 'uncached';
 
+
+    /**
+     * @Flow\InjectConfiguration()
+     * @var array
+     */
+    protected $_configuration;
+
     /**
      *
      * @param \TYPO3\Flow\AOP\JoinPointInterface $joinPoint The current join point
      * @return mixed Result of the target method
-     * @Flow\Before("method(TYPO3\TypoScript\Core\Cache\ContentCache->createCacheSegment())")
+     * @Flow\Before("method(TYPO3\TypoScript\Core\Cache\ContentCache->createCacheSegment()) && setting(VIVOMEDIA.TypoScript.CacheVisualisation.enabled)")
      */
-    public function enrichContentCacheCachedSegment(\TYPO3\Flow\AOP\JoinPointInterface $joinPoint)
+    public function wrapContentCacheCachedSegment(\TYPO3\Flow\AOP\JoinPointInterface $joinPoint)
     {
         $content = $joinPoint->getMethodArgument('content');
-        $parameter = [
-            'path' => $joinPoint->getMethodArgument('typoScriptPath'),
-            'tags' => implode(',', $joinPoint->getMethodArgument('tags')),
-            'lifetime' => $joinPoint->getMethodArgument('lifetime')
-        ];
-        $content = $this->_wrapContent(self::TYPE_CACHED, $content, $parameter);
+        $path = $joinPoint->getMethodArgument('typoScriptPath');
 
-        $joinPoint->setMethodArgument('content', $content);
+        if (!$this->_checkBlacklistedPath($path)) {
+            $parameter = [
+                'path' => $path,
+                'tags' => implode(',', $joinPoint->getMethodArgument('tags')),
+                'lifetime' => $joinPoint->getMethodArgument('lifetime')
+            ];
+            $content = $this->_wrapContent(self::TYPE_CACHED, $content, $parameter);
+
+            $joinPoint->setMethodArgument('content', $content);
+        }
     }
 
     /**
      *
      * @param \TYPO3\Flow\AOP\JoinPointInterface $joinPoint The current join point
      * @return mixed Result of the target method
-     * @Flow\Around("method(TYPO3\TypoScript\Core\Cache\ContentCache->createUncachedSegment())")
+     * @Flow\Around("method(TYPO3\TypoScript\Core\Cache\ContentCache->createUncachedSegment()) && setting(VIVOMEDIA.TypoScript.CacheVisualisation.enabled)")
      */
-    public function enrichContentCacheUncachedSegment(\TYPO3\Flow\AOP\JoinPointInterface $joinPoint)
+    public function wrapContentCacheUncachedSegment(\TYPO3\Flow\AOP\JoinPointInterface $joinPoint)
     {
         $content = $joinPoint->getAdviceChain()->proceed($joinPoint);
-        $parameter = [
-            'Path' => $joinPoint->getMethodArgument('typoScriptPath')
-        ];
-        $content = $this->_wrapContent(self::TYPE_CACHED, $content, $parameter);
+        $path = $joinPoint->getMethodArgument('typoScriptPath');
 
+        if (!$this->_checkBlacklistedPath($path)) {
+            $parameter = [
+                'path' => $path,
+                'context' => array_keys($joinPoint->getMethodArgument('contextVariables'))
+            ];
+            $content = $this->_wrapContent(self::TYPE_CACHED, $content, $parameter);
+        }
         return $content;
     }
 
     private function _wrapContent($type, $content, $parameter)
     {
-        // default class
-        // class cached
-        // class uncached
-        // style cached
-        // style uncached
+        $wrapperClasses = [
+            'default' => $this->_configuration['output']['wrapperClassName'],
+            'item' => $this->_configuration['output']['itemClassName'][$type]
+        ];
+        $content = '<div class="' . implode(' ', $wrapperClasses) . '" data-vivomedia-cache-visualisation=\'' . json_encode($parameter) . '\'>' . $content . '</div>';
+        return $content;
+    }
 
-        $wrappedContent = '<div class="vivomedia-cachevisualisation vivomedia-cachevisualisation-cached" style="border: thin solid green;">';
-        $wrappedContent .= '<div class="vivomedia-cachevisualisation-info-container" style="display: node">'.var_export($parameter, true).'</div>';
-        $wrappedContent .= $content;
-        $wrappedContent .= '</div>';
-        return $wrappedContent;
+    private function _checkBlacklistedPath($path)
+    {
+        // TODO: mv to config
+        $blacklist = [
+            'root',
+            'root<TYPO3.TypoScript:Case>/default<TYPO3.TypoScript:Matcher>/element<TYPO3.Neos.NodeTypes:Page.Document>'
+        ];
+        return in_array($path, $blacklist);
     }
 }
