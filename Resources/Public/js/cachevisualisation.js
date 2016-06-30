0bfe444cb75bd705343852c1f0cab99b063c434a
Denny Lubitz <lubitz@vivomedia.de>
2016-06-30 13:30:07 +0200

    Added CSS and JS for styled output

---
+++ Resources/Public/js/cachevisualisation.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+  $('div[data-vivomedia-cache-visualisation]').hover(
+    function() {
+      var container = $( this );
+      $('> .data-container', container).show();
+    },
+    function(){
+      var container = $( this );
+      $('> .data-container', container).hide();
+  });
+
+
+  $('div[data-vivomedia-cache-visualisation]').each(function () {
+    var wrapper = $(this);
+    var dataContainer = $('<div class="data-container"></div>');
+    wrapper.prepend(dataContainer);
+
+    var data = wrapper.data('vivomedia-cache-visualisation');
+    $.each(data, function(key, value){
+      dataContainer.append($('<label />').text(key));
+      console.log(value);
+      console.log($.isArray(value));
+      if ($.isArray(value)) {
+        var list = $('<ul />');
+        $.each(value, function(arrayKey, arrayValue) {
+          list.append($('<li />').text(arrayValue));
+        });
+        dataContainer.append(list);
+      } else {
+        if (value == null) {
+          value = 'null';
+        }
+        dataContainer.append($('<p />').text(value));
+      }
+    });
+  });
+})
